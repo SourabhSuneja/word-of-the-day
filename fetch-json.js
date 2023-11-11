51b85197bed04da5d38508d579ffcb3612d4a501
SourabhSuneja <78584922+SourabhSuneja@users.noreply.github.com>
2023-11-11 14:52:16 +0530

    Update fetch-json.js

--- fetch-json.js
+++ fetch-json.js
@@ -1,48 +1,14 @@
-//JSON Dictionary
-
-
-
-
-
-// fetching code starts
-
 document.getElementById('loadFromJson').addEventListener('click', function () {
-            // Check if the checkbox is checked
-            var useJsonDataCheckbox = document.getElementById('useJsonData');
-            if (useJsonDataCheckbox.checked) {
-                // Parse the provided JSON data
-
-
-
-
-var jsonData = {
-  "words": [
-    {
-      "word": "Ambiguous",
-      "meaning": "Having more than one possible interpretation.",
-      "synonym": "Uncertain",
-      "antonym": "Clear",
-      "sentence": "The instructions were ambiguous, leaving the students confused about the assignment.",
-      "pronunciation": "am-big-yoo-uhs"
-    },
-    {
-      "word": "Cacophony",
-      "meaning": "A harsh, discordant mixture of sounds.",
-      "synonym": "Noise",
-      "antonym": "Harmony",
-      "sentence": "The cacophony of honking horns made it hard to concentrate in the busy city.",
-      "pronunciation": "kuh-kaw-fuh-nee"
-    }]};
-
-
-
-
-
-
-    
+    // Check if the checkbox is checked
+    var useJsonDataCheckbox = document.getElementById('useJsonData');
+    if (useJsonDataCheckbox.checked) {
+        // Fetch the external JSON data
+        fetch('data.json')
+            .then(response => response.json())
+            .then(jsonData => {
                 // Pick a random word
-                // var randomIndex = Math.floor(Math.random() * jsonData.words.length); 
-                var randomWord = jsonData.words[0];
+                var randomIndex = Math.floor(Math.random() * jsonData.words.length);
+                var randomWord = jsonData.words[randomIndex];
 
                 // Insert data into corresponding input fields
                 document.getElementById('word').value = randomWord.word;
@@ -51,6 +17,9 @@ var jsonData = {
                 document.getElementById('antonym').value = randomWord.antonym;
                 document.getElementById('sentence').value = randomWord.sentence;
                 document.getElementById('pronunciation').value = randomWord.pronunciation;
-            } else {
-                alert("Please check the 'Use pre-loaded JSON data' checkbox.");
-            }
+            })
+            .catch(error => console.error('Error fetching JSON:', error));
+    } else {
+        alert("Please check the 'Use pre-loaded JSON data' checkbox.");
+    }
+});
