79afa92e62ee909675a513265c1d6e3a24840bce
SourabhSuneja <78584922+SourabhSuneja@users.noreply.github.com>
2023-11-11 12:23:03 +0530

    Created fetch-json.js

---
+++ fetch-json.js
@@ -0,0 +1,22 @@
+document.getElementById('loadFromJson').addEventListener('click', function () {
+            // Check if the checkbox is checked
+            var useJsonDataCheckbox = document.getElementById('useJsonData');
+            if (useJsonDataCheckbox.checked) {
+                // Parse the provided JSON data
+                var jsonData = /* Paste the provided JSON data here */;
+                
+                // Pick a random word
+                var randomIndex = Math.floor(Math.random() * jsonData.words.length);
+                var randomWord = jsonData.words[randomIndex];
+
+                // Insert data into corresponding input fields
+                document.getElementById('word').value = randomWord.word;
+                document.getElementById('meaning').value = randomWord.meaning;
+                document.getElementById('synonym').value = randomWord.synonym;
+                document.getElementById('antonym').value = randomWord.antonym;
+                document.getElementById('sentence').value = randomWord.sentence;
+                document.getElementById('pronunciation').value = randomWord.pronunciation;
+            } else {
+                alert("Please check the 'Use pre-loaded JSON data' checkbox.");
+            }
+        });
