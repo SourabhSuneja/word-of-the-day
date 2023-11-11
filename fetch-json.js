595ab161ade4aa3edc085e108cf5531c138399b2
SourabhSuneja <78584922+SourabhSuneja@users.noreply.github.com>
2023-11-11 14:20:04 +0530

    Update fetch-json.js

--- fetch-json.js
+++ fetch-json.js
@@ -1,5 +1,6 @@
 //JSON Dictionary
 
+
 var jsonData = {
   "words": [
     {
@@ -17,193 +18,9 @@ var jsonData = {
       "antonym": "Harmony",
       "sentence": "The cacophony of honking horns made it hard to concentrate in the busy city.",
       "pronunciation": "kuh-kaw-fuh-nee"
-    },
-    {
-      "word": "Debilitate",
-      "meaning": "To make weak or feeble.",
-      "synonym": "Weaken",
-      "antonym": "Strengthen",
-      "sentence": "The illness debilitated her to the point that she couldn't get out of bed.",
-      "pronunciation": "dih-bil-i-teyt"
-    },
-    {
-      "word": "Eloquent",
-      "meaning": "Fluent or persuasive in speaking or writing.",
-      "synonym": "Articulate",
-      "antonym": "Inarticulate",
-      "sentence": "The president gave an eloquent speech that moved the audience.",
-      "pronunciation": "el-uh-kwuhnt"
-    },
-    {
-      "word": "Facetious",
-      "meaning": "Treating serious issues with inappropriate humor.",
-      "synonym": "Witty",
-      "antonym": "Serious",
-      "sentence": "His facetious remarks during the meeting were not well-received.",
-      "pronunciation": "fuh-see-shuhs"
-    },
-    {
-      "word": "Galvanize",
-      "meaning": "To shock or excite someone, typically into taking action.",
-      "synonym": "Stimulate",
-      "antonym": "Deter",
-      "sentence": "The inspiring speech galvanized the community to work together for a common goal.",
-      "pronunciation": "gal-vuh-nahyz"
-    },
-    {
-      "word": "Harangue",
-      "meaning": "A lengthy and aggressive speech.",
-      "synonym": "Tirade",
-      "antonym": "Praise",
-      "sentence": "The teacher delivered a harangue about the importance of studying for exams.",
-      "pronunciation": "huh-rang"
-    },
-    {
-      "word": "Ineffable",
-      "meaning": "Too great or extreme to be expressed in words.",
-      "synonym": "Indescribable",
-      "antonym": "Expressible",
-      "sentence": "The beauty of the sunset was ineffable, leaving everyone in awe.",
-      "pronunciation": "in-ef-uh-buhl"
-    },
-    {
-      "word": "Juxtapose",
-      "meaning": "To place or deal with close together for contrasting effect.",
-      "synonym": "Contrast",
-      "antonym": "Separate",
-      "sentence": "The artist chose to juxtapose light and dark colors in the painting.",
-      "pronunciation": "juhk-stuh-pohz"
-    },
-    {
-      "word": "Kaleidoscope",
-      "meaning": "A constantly changing pattern or sequence of elements.",
-      "synonym": "Array",
-      "antonym": "Monotony",
-      "sentence": "The city at night was a kaleidoscope of lights and sounds.",
-      "pronunciation": "kuh-lahy-duh-skohp"
-    },
-    {
-      "word": "Lethargic",
-      "meaning": "Sluggish and apathetic.",
-      "synonym": "Drowsy",
-      "antonym": "Energetic",
-      "sentence": "After a big meal, the students felt lethargic and struggled to focus.",
-      "pronunciation": "luh-thahr-jik"
-    },
-    {
-      "word": "Malleable",
-      "meaning": "Capable of being shaped or molded.",
-      "synonym": "Pliable",
-      "antonym": "Rigid",
-      "sentence": "Clay is a malleable material that artists use to create various sculptures.",
-      "pronunciation": "mal-ee-uh-buhl"
-    },
-    {
-      "word": "Nefarious",
-      "meaning": "Wicked, villainous, or criminal.",
-      "synonym": "Evil",
-      "antonym": "Virtuous",
-      "sentence": "The nefarious plot of the villain was finally exposed and thwarted.",
-      "pronunciation": "ni-fair-ee-uhs"
-    },
-    {
-      "word": "Obliterate",
-      "meaning": "To destroy utterly; wipe out.",
-      "synonym": "Annihilate",
-      "antonym": "Preserve",
-      "sentence": "The powerful storm threatened to obliterate the small village.",
-      "pronunciation": "uh-blit-uh-reyt"
-    },
-    {
-      "word": "Pernicious",
-      "meaning": "Having a harmful effect, especially in a gradual or subtle way.",
-      "synonym": "Destructive",
-      "antonym": "Beneficial",
-      "sentence": "Gossip can have a pernicious impact on relationships.",
-      "pronunciation": "per-nish-uhs"
-    },
-    {
-      "word": "Quell",
-      "meaning": "To suppress or extinguish something.",
-      "synonym": "Subdue",
-      "antonym": "Incite",
-      "sentence": "The principal worked to quell the rumors circulating in the school.",
-      "pronunciation": "kwel"
-    },
-    {
-      "word": "Ratify",
-      "meaning": "To formally approve or accept.",
-      "synonym": "Confirm",
-      "antonym": "Reject",
-      "sentence": "The countries agreed to ratify the treaty, promoting peace.",
-      "pronunciation": "rat-uh-fahy"
-    },
-    {
-      "word": "Sycophant",
-      "meaning": "A person who acts obsequiously toward someone important to gain advantage.",
-      "synonym": "Flatterer",
-      "antonym": "Rebel",
-      "sentence": "The sycophant constantly praised the boss to win favor.",
-      "pronunciation": "sik-uh-fuhnt"
-    },
-    {
-      "word": "Trepidation",
-      "meaning": "A feeling of fear or agitation about something that may happen.",
-      "synonym": "Apprehension",
-      "antonym": "Confidence",
-      "sentence": "With trepidation, he approached the haunted house on Halloween night.",
-      "pronunciation": "trep-i-dey-shuhn"
-    },
-    {
-      "word": "Ubiquitous",
-      "meaning": "Present, appearing, or found everywhere.",
-      "synonym": "Pervasive",
-      "antonym": "Rare,
-      "sentence": "In the modern world, smartphones are ubiquitous.",
-      "pronunciation": "yoo-bi-kwuh-tuhs"
-    },
-    {
-      "word": "Voracious",
-      "meaning": "Wanting or devouring great quantities of food.",
-      "synonym": "Insatiable",
-      "antonym": "Moderate",
-      "sentence": "After the hike, they had a voracious appetite and finished the entire pizza.",
-      "pronunciation": "vuh-rey-shuhs"
-    },
-    {
-      "word": "Whimsical",
-      "meaning": "Playfully quaint or fanciful, especially in an appealing and amusing way.",
-      "synonym": "Playful",
-      "antonym": "Serious",
-      "sentence": "The whimsical design of the amusement park attracted visitors of all ages.",
-      "pronunciation": "hwim-si-kuhl"
-    },
-    {
-      "word": "Xenophobia",
-      "meaning": "Dislike or prejudice against people from other countries.",
-      "synonym": "Nativism",
-      "antonym": "Open-mindedness",
-      "sentence": "Xenophobia can hinder cultural understanding and harmony.",
-      "pronunciation": "zen-uh-foh-bee-uh"
-    },
-    {
-      "word": "Yearn",
-      "meaning": "To have an intense feeling of longing for something.",
-      "synonym": "Crave",
-      "antonym": "Abandon",
-      "sentence": "The explorer yearned to discover new lands and cultures.",
-      "pronunciation": "yurn"
-    },
-    {
-      "word": "Zealot",
-      "meaning": "A person who is fanatical and uncompromising in pursuit of their religious, political, or other ideals.",
-      "synonym": "Fanatic",
-      "antonym": "Moderate",
-      "sentence": "The political zealot passionately campaigned for his candidate.",
-      "pronunciation": "zel-uht"
     }
-  ]
-}; 
+]
+};
 
 
 alert("Hi");
@@ -232,4 +49,21 @@ alert(jsonData);
             } else {
                 alert("Please check the 'Use pre-loaded JSON data' checkbox.");
             }
-        });
+        var jsonData = {
+  "words": [
+    {
+      "word": "Ambiguous",
+      "meaning": "Having more than one possible interpretation.",
+      "synonym": "Uncertain",
+      "antonym": "Clear",
+      "sentence": "The instructions were ambiguous, leaving the students confused about the assignment.",
+      "pronunciation": "am-big-yoo-uhs"
+    },
+    {
+      "word": "Cacophony",
+      "meaning": "A harsh, discordant mixture of sounds.",
+      "synonym": "Noise",
+      "antonym": "Harmony",
+      "sentence": "The cacophony of honking horns made it hard to concentrate in the busy city.",
+      "pronunciation": "kuh-kaw-fuh-nee"
+    }
