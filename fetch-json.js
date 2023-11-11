591a71302142a105e2fdb36c676c96fc4c6f316c
SourabhSuneja <78584922+SourabhSuneja@users.noreply.github.com>
2023-11-11 13:35:47 +0530

    Update fetch-json.js

--- fetch-json.js
+++ fetch-json.js
@@ -1,36 +1,12 @@
 document.getElementById('loadFromJson').addEventListener('click', function () {
             // Check if the checkbox is checked
-alert('hi');
             var useJsonDataCheckbox = document.getElementById('useJsonData');
             if (useJsonDataCheckbox.checked) {
                 // Parse the provided JSON data
-
-
-
-
-
-var jsonData = {
-  "words": [
-    {
-      "word": "Ambiguous",
-      "meaning": "Having more than one possible interpretation.",
-      "synonym": "Uncertain",
-      "antonym": "Clear",
-      "sentence": "The instructions were ambiguous, leaving the students confused about the assignment.",
-      "pronunciation": "am-big-yoo-uhs"
-    }
-]
-};
-
-
-
-alert("run");
-
-
     
                 // Pick a random word
-                var randomIndex = Math.floor(Math.random() * jsonData.words.length); randomIndex=0;
-                var randomWord = jsonData.words[0];
+                var randomIndex = Math.floor(Math.random() * jsonData.words.length); 
+                var randomWord = jsonData.words[randomIndex];
 
                 // Insert data into corresponding input fields
                 document.getElementById('word').value = randomWord.word;
