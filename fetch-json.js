cc01b69d52ffb90cb25919b7f62bf5dd10e149a6
SourabhSuneja <78584922+SourabhSuneja@users.noreply.github.com>
2023-11-13 10:57:20 +0530

    Update fetch-json.js

--- fetch-json.js
+++ fetch-json.js
@@ -1,5 +1,5 @@
 // Array of JSON file sources
-var jsonSources = ['data.json', 'data2.json']; // Add more sources as needed
+var jsonSources = ['data1.json', 'data2.json', 'data3.json']; // Add more sources as needed
 var jsonDataCache = {}; // Cache to store pre-loaded JSON data
 
 // Function to fetch and cache JSON data
@@ -12,42 +12,42 @@ function fetchAndCacheJsonData(source) {
         .catch(error => console.error(`Error fetching JSON from ${source}:`, error));
 }
 
-// Pre-load JSON data for all sources
-jsonSources.forEach(source => fetchAndCacheJsonData(source));
+// Use Promise.all to wait for all fetch operations to complete
+Promise.all(jsonSources.map(source => fetchAndCacheJsonData(source)))
+    .then(() => {
+        // All JSON files have been fetched and cached
 
-document.getElementById('loadFromJson').addEventListener('click', function () { 
-    // Check if the checkbox is checked
-    var useJsonDataCheckbox = document.getElementById('useJsonData');
-    if (useJsonDataCheckbox.checked) {
-        // Randomly select a JSON file source
-        var randomSource = jsonSources[Math.floor(Math.random() * jsonSources.length)];
-        
-        // Use the pre-loaded JSON data from the cache
-        var jsonData = jsonDataCache[randomSource];
+        // Attach event handler after all JSON files have been loaded
+        document.getElementById('loadFromJson').addEventListener('click', function () {
+            // Event handler logic
 
-        if (jsonData) {
-            // Pick a random word
-            var randomIndex = Math.floor(Math.random() * jsonData.words.length);
-            var randomWord = jsonData.words[randomIndex];
+            // Randomly select a JSON file source
+            var randomSource = jsonSources[Math.floor(Math.random() * jsonSources.length)];
 
-            // Insert data into corresponding input fields
-            document.getElementById('word').value = randomWord.word;
-            document.getElementById('meaning').value = randomWord.meaning;
-            document.getElementById('synonym').value = randomWord.synonym;
-            document.getElementById('antonym').value = randomWord.antonym;
-            document.getElementById('sentence').value = randomWord.sentence;
-            document.getElementById('pronunciation').value = randomWord.pronunciation;
-        } else {
-            console.error(`Error: JSON data for ${randomSource} not pre-loaded.`);
+            // Use the pre-loaded JSON data from the cache
+            var jsonData = jsonDataCache[randomSource];
+
+            if (jsonData) {
+                // Pick a random word
+                var randomIndex = Math.floor(Math.random() * jsonData.words.length);
+                var randomWord = jsonData.words[randomIndex];
+
+                // Insert data into corresponding input fields
+                document.getElementById('word').value = randomWord.word;
+                document.getElementById('meaning').value = randomWord.meaning;
+                document.getElementById('synonym').value = randomWord.synonym;
+                document.getElementById('antonym').value = randomWord.antonym;
+                document.getElementById('sentence').value = randomWord.sentence;
+                document.getElementById('pronunciation').value = randomWord.pronunciation;
+            } else {
+                console.error(`Error: JSON data for ${randomSource} not pre-loaded.`);
+            }
+        });
+
+        // Trigger click on loadFromJson button
+        var useJsonDataCheckbox = document.getElementById('useJsonData');
+        if (useJsonDataCheckbox.checked) {
+            document.getElementById('loadFromJson').click();
         }
-    } else {
-        alert("Please check the 'Use pre-loaded JSON data' checkbox.");
-    }
-});
-
-var useJsonDataCheckbox = document.getElementById('useJsonData');
-if (useJsonDataCheckbox.checked) {
-    document.getElementById('loadFromJson').click();
-}
-
-
+    })
+    .catch(error => console.error('Error fetching JSON:', error));
